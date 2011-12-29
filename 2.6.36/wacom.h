ea43c99da4103f9812a006919780080c1341b821
Chris Bagwell <chris@cnpbagwell.com>
2011-12-29 17:26:23 -0600

    Add missing input_mt_get_value() for 2.6.36/37
    Reported in bug tracker #3463769.
    
    Based on mini code review, I think this is only missing
    symbol and 2.6.36/37 contain ABS_MT_FIRST and input_mt_slot
    which this function depends on.
    
    I'm unable to test this fix but its a copy&paste of
    function from mt.h in later kernels.

--- 2.6.36/wacom.h
+++ 2.6.36/wacom.h
@@ -94,6 +94,12 @@
 # endif 
 #if LINUX_VERSION_CODE > KERNEL_VERSION(2,6,37)
 #include <linux/input/mt.h>
+#else
+static inline int input_mt_get_value(const struct input_mt_slot *slot,
+                                     unsigned code)
+{
+        return slot->abs[code - ABS_MT_FIRST];
+}
 #endif
 #include <asm/unaligned.h>
 
