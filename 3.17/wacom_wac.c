6c04773b0e366bbadc7476bedea91ca8bb813d0d
Ping Cheng <pingc@wacom.com>
2015-01-23 17:41:00 -0800

    Make sure touch arbitration is applied consistently across devices
    stylus_in_proximity is used to make sure no touch event is sent while pen
    is in proximity. touch_down is used to make sure a touch up event is sent
    when pen comes into proximity while touch is down.
    
    Two touch routines didn't store touch_down. One touch routine forgot to
    check stylus_in_proximity before sending touch events. This patch fixes
    those issues.
    
    Signed-off-by: Ping Cheng <pingc@wacom.com>

--- 3.17/wacom_wac.c
+++ 3.17/wacom_wac.c
@@ -1040,7 +1040,7 @@ static int wacom_24hdt_irq(struct wacom_wac *wacom)
 
 	for (i = 0; i < contacts_to_send; i++) {
 		int offset = (WACOM_BYTES_PER_24HDT_PACKET * i) + 1;
-		bool touch = data[offset] & 0x1 && !wacom->shared->stylus_in_proximity;
+		bool touch = (data[offset] & 0x1) && !wacom->shared->stylus_in_proximity;
 		int slot = input_mt_get_slot_by_key(input, data[offset + 1]);
 
 		if (slot < 0)
@@ -1070,6 +1070,7 @@ static int wacom_24hdt_irq(struct wacom_wac *wacom)
 	if (wacom->num_contacts_left <= 0)
 		wacom->num_contacts_left = 0;
 
+	wacom->shared->touch_down = (wacom->num_contacts_left > 0);
 	return 1;
 }
 
@@ -1098,7 +1099,7 @@ static int wacom_mt_touch(struct wacom_wac *wacom)
 
 	for (i = 0; i < contacts_to_send; i++) {
 		int offset = (WACOM_BYTES_PER_MT_PACKET + x_offset) * i + 3;
-		bool touch = data[offset] & 0x1;
+		bool touch = (data[offset] & 0x1) && !wacom->shared->stylus_in_proximity;
 		int id = get_unaligned_le16(&data[offset + 1]);
 		int slot = input_mt_get_slot_by_key(input, id);
 
@@ -1120,6 +1121,7 @@ static int wacom_mt_touch(struct wacom_wac *wacom)
 	if (wacom->num_contacts_left < 0)
 		wacom->num_contacts_left = 0;
 
+	wacom->shared->touch_down = (wacom->num_contacts_left > 0);
 	return 1;
 }
 
