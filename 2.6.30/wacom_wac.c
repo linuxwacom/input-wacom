b863a16add51aef5f7b8c6513512ef27cef83a6e
Jason Gerecke <killertofu@gmail.com>
2015-04-29 13:56:46 -0700

    2.6.30: Fix ISDv4 names
    Signed-off-by: Jason Gerecke <jason.gerecke@wacom.com>

--- 2.6.30/wacom_wac.c
+++ 2.6.30/wacom_wac.c
@@ -2066,11 +2066,11 @@ static const struct wacom_features wacom_features_0x12C =
 static const struct wacom_features wacom_features_0x4001 =
 	{ "Wacom ISDv4 4001",       WACOM_PKGLEN_MTTPC,   26202, 16325,  255,  0, MTTPC };
 static const struct wacom_features wacom_features_0x4004 =
-	{ "Wacom ISDv4 4001",       WACOM_PKGLEN_MTTPC,   11060, 6220,  255,  0, MTTPC_B };
+	{ "Wacom ISDv4 4004",       WACOM_PKGLEN_MTTPC,   11060, 6220,  255,  0, MTTPC_B };
 static const struct wacom_features wacom_features_0x5000 =
-	{ "Wacom ISDv4 4001",       WACOM_PKGLEN_MTTPC,   27848, 15752,  255,  0, MTTPC_B };
+	{ "Wacom ISDv4 5000",       WACOM_PKGLEN_MTTPC,   27848, 15752,  255,  0, MTTPC_B };
 static const struct wacom_features wacom_features_0x5002 =
-	{ "Wacom ISDv4 4001",       WACOM_PKGLEN_MTTPC,   29576, 16724,  1023,  0, MTTPC_B };
+	{ "Wacom ISDv4 5002",       WACOM_PKGLEN_MTTPC,   29576, 16724,  1023,  0, MTTPC_B };
 static const struct wacom_features wacom_features_0x47 =
 	{ "Wacom Intuos2 6x8",    WACOM_PKGLEN_INTUOS,    20320, 16240, 1023, 31, INTUOS };
 static const struct wacom_features wacom_features_0x6004 =
