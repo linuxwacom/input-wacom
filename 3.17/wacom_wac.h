910a76bba802a34d9848e16054efdadf0ced8e2b
Ping Cheng <ping.cheng@wacom.com>
2021-07-19 13:55:29 -0700

    backport: HID: wacom: set initial hardware touch switch state to 'off'
    Wacom touch devices have two types of touch switches: softkey touch
    toggle and hardware touch switch. For softkey toggle, we assume
    touch is on by default in the driver. However the hardware touch
    switch is controlled by end users. We don't know if it's on or off
    before getting the status event.
    
    This patch sets touch off for devices with a hardware switch until we
    get the status. This is a bit safer for users who leave the switch "off"
    and don't want any accidental touches. The tradeoff is a slight delay
    between device connection and touch becoming enabled for users who
    leave the switch "on".
    
    Signed-off-by: Ping Cheng <ping.cheng@wacom.com>
    Reviewed-by: Jason Gerecke <jason.gerecke@wacom.com>
    Tested-by: Jason Gerecke <jason.gerecke@wacom.com>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    [jason.gerecke@wacom.com: Imported into input-wacom repository (dc9dc864f35d)]
    Signed-off-by: Jason Gerecke <jason.gerecke@wacom.com>
    [jason.gerecke@wacom.com: Backported from input-wacom repository (13c1b93814b3)]
    Signed-off-by: Jason Gerecke <jason.gerecke@wacom.com>

--- 3.17/wacom_wac.h
+++ 3.17/wacom_wac.h
@@ -373,6 +373,7 @@ struct wacom_wac {
 	struct work_struct intuos_prox_event_worker;
 	struct hid_data hid_data;
 	bool has_mute_touch_switch;
+	bool is_soft_touch_switch;
 	bool has_mode_change;
 	bool is_direct_mode;
 	bool is_invalid_bt_frame;
