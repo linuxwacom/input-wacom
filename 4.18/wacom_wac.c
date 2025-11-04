831a051b0fa0ad52e2d379e7f383c970786391e5
Tatsunosuke Tobita <tatsunosuke.wacom@gmail.com>
2025-11-04 18:16:29 +0900

    treewide, timers: Rename from_timer() to timer_container_of() Move this API to the canonical timer_*() namespace.
    [ tglx: Redone against pre rc1 ]
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/all/aB2X0jCKQO56WdMt@gmail.com

--- 4.18/wacom_wac.c
+++ 4.18/wacom_wac.c
@@ -75,7 +75,11 @@ static void wacom_force_proxout(struct wacom_wac *wacom_wac)
 
 void wacom_idleprox_timeout(struct timer_list *list)
 {
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6,16,0)
 	struct wacom *wacom = from_timer(wacom, list, idleprox_timer);
+#else
+	struct wacom *wacom = timer_container_of(wacom, list, idleprox_timer);
+#endif
 	struct wacom_wac *wacom_wac = &wacom->wacom_wac;
 
 	if (!wacom_wac->hid_data.sense_state) {
